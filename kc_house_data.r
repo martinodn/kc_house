761e77207ff0a2702d372876c5d3214f577824cb
damiclem <damiano.clementel@gmail.com>
2019-05-07 22:39:37 +0200

    Added dataset file; Added check on ids and dates

--- kc_house_data.r
+++ kc_house_data.r
@@ -2,8 +2,6 @@ kc_house<-read.csv("kc_house_data.csv")
 attach(kc_house)
 
 summary(kc_house)
-#id not useful to understand data and to predict
-kc_house<-kc_house[-1]
 
 #check the dimension of the dataset
 dim(kc_house)
@@ -18,3 +16,16 @@ new.date<-new.date-(sort(new.date))[1]
 kc_house[1]<-new.date
 kc_house
 dim(kc_house)
+
+#check the number of duplicated ids
+length(id) - length(unique(id))
+#since id is not unique, we retrieve rows with duplicated id
+d<-kc_house[duplicated(id),]
+dim(d)
+#we note that there are some duplicates wrt id and date
+#we suppose, by observations, that a specific id corresponds to a specific date
+d<-kc_house[duplicated(id, date),]
+dim(d)
+#our observation is confirmed by the check on the count of the duplicates
+#hence, id not useful to understand data and to predict
+kc_house<-kc_house[-1]
